dd78d3c9da9ce77630391411a057f8c92bd2a573
Kevin Lanzon <kevinlanzon@gmail.com>
2015-02-25 16:39:49 +0000

    added filtering by tag

--- app/server.rb
+++ app/server.rb
@@ -28,6 +28,12 @@ class BookmarkManager < Sinatra::Base
       redirect to('/')
   end
 
+  get '/tags/:text' do
+    tag = Tag.first(:text => params[:text])
+    @links = tag ? tag.links : []
+    erb :index
+  end
+
 end
 
 
